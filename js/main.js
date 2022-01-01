8c4dc534aca65c99d0078bc9c07be7f75a505b56
Egor Potebenko <hitechnic.uu68@gmail.com>
2022-01-02 01:15:11 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-!function(){const e=document.querySelector(".header");var o=e.offsetTop;window.onscroll=()=>{window.pageYOffset>=o?e.classList.add("header__sticky"):e.classList.remove("header__sticky")}}(),$(document).ready((function(){$(".intro__burger").click((function(e){$(".intro__burger, .intro__nav").toggleClass("active"),$("body").toggleClass("lock")}))}));
+// Sticky nav menu
+(function() {
+
+    const header = document.querySelector(".header");
+    var sticky = header.offsetTop;
+
+    window.onscroll = () => {
+        if (window.pageYOffset >= sticky) {
+            header.classList.add("header__sticky");
+        } else {
+            header.classList.remove("header__sticky");
+        }
+    };
+}());
+
+// Burger handle
+$(document).ready(function() {
+    $('.intro__burger').click(function(event) {
+        $('.intro__burger, .intro__nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+} )
